099473d5d1ce89ddd4b1fb33479375dbbbbe7260
Rafael Frias <rfriasge@gmail.com>
2020-08-29 18:22:55 -0400

    First commit

---
+++ src/PrimeraApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+    //const saludo = "Hola mundo ";
+    return (
+        <>
+            <h1> { saludo } </h1>
+            <p>{ subtitulo }</p>
+        </>
+    )
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string
+}
+PrimeraApp.defaultProps = {
+    subtitulo : "Soy un subtitulo"
+}
+export default PrimeraApp;
