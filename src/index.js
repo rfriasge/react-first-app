1ac1882659bb8c31314886da352eb0f85bb6b45e
Rafael Frias <rfriasge@gmail.com>
2020-08-30 08:10:02 -0400

    index.js modificado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PrimeraApp from './PrimeraApp'
 import './index.css';
-
+// Comentario
 const divRoot = document.querySelector("#root");
 
 ReactDOM.render(<PrimeraApp saludo="123" />, divRoot);
